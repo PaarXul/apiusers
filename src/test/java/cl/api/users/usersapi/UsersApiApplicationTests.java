2fda9abf2cda04ace9210b43ac336921a32f0767
PaarXul <gian.parra96@gmail.com>
2023-09-07 23:30:45 -0300

    Comenzando el proyecto

---
+++ src/test/java/cl/api/users/usersapi/UsersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.api.users.usersapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
